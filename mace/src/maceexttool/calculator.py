948382ce4338f3d2fae5e83ea7b10f0f52c36144
LesTuche <joel@eloh.ch>
2025-07-11 11:04:31 +0200

    model was initallized with flaot32 changed to float64

--- mace/src/maceexttool/calculator.py
+++ mace/src/maceexttool/calculator.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import warnings
 import os
-MODEL_PATH ="/cluster/home/jlandis/ml_potentials/MACE-matpes-r2scan-omat-ft.model"
+MODEL_PATH = "/cluster/home/jlandis/ml_potentials/MACE-matpes-r2scan-omat-ft.model"
 
 with warnings.catch_warnings():
     # suppress the missing PySisiphus warning
@@ -13,11 +13,12 @@ with warnings.catch_warnings():
 
 def init(model):
     """Initialize an MACE r2scan foundation model instance."""
-    print(f"Initializing MACE calculator with model: {model} for optimizations")
-    
+    print(
+        f"Initializing MACE calculator with model: {model} for optimizations")
+
     # Try the foundation model first, fall back to local model loading if it fails
     try:
-        return mace_mp(model=MODEL_PATH, device="cpu", dtype="float64")
+        return mace_mp(model=MODEL_PATH, device="cpu", default_dtype="float64")
     except Exception as e:
         print(f"Failed to load with mace_mp: {e}")
         print("Falling back to MACECalculator...")
